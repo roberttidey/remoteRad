8604c4025486f80989a0af0288214fb9a64cf4e1
Robert Tidey <robert@tideys.net>
2016-02-03 08:53:50 +0000

    Publish method to control via Web

--- RadTxTest.ino
+++ RadTxTest.ino
@@ -1,22 +1,62 @@
-#include <RadTx.h>
-//on,mode,mode,mode,delay1200,off,delay1200,end
-uint16_t msg[] = {0x027e,0x026f,0x026f,0x026f,0x84b0,0x027e,0x84b0,0xffff};
+// This #include statement was automatically added by the Particle IDE.
+#include "RadTx.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "SparkIntervalTimer/SparkIntervalTimer.h"
+
+#define MAX_MSGS 10
+#define MSG_LEN 10
+// msg0 on-off,end
+// msg1 temp,end
+// msg2 mode,end
+// msg3 time,end
+// msg4 down,end
+// msg5 up,end
+// msg6 on,mode,mode,mode,delay1200,off,delay1200,end
+// msg7 on,mode,mode,mode,delay1200,off,delay1200,end
+// msg8 on,mode,mode,mode,delay1200,off,delay1200,end
+// msg9 on,mode,mode,mode,delay1200,off,delay1200,end
+uint16_t msg[MSG_LEN * MAX_MSGS] = {0x027e,0xffff,0,0,0,0,0,0,0,0,
+                                    0x0277,0xffff,0,0,0,0,0,0,0,0,
+                                    0x026f,0xffff,0,0,0,0,0,0,0,0,
+                                    0x0277,0xffff,0,0,0,0,0,0,0,0,
+                                    0x0239,0xffff,0,0,0,0,0,0,0,0,
+                                    0x027d,0xffff,0,0,0,0,0,0,0,0,
+                                    0x027e,0x026f,0x026f,0x026f,0x84b0,0x027e,0x84b0,0xffff,0,0,
+                                    0x027e,0x026f,0x026f,0x026f,0x84b0,0x027e,0x84b0,0xffff,0,0,
+                                    0x027e,0x026f,0x026f,0x026f,0x84b0,0x027e,0x84b0,0xffff,0,0,
+                                    0x027e,0x026f,0x026f,0x026f,0x84b0,0x027e,0x84b0,0xffff,0,0,
+};
 long timeout = 0;
+int sendMsg(String command);
+int LED = D7;
+int IRLED = D0;
 
 void setup() {
-  //Transmit on pin 7, 10 repeats,no invert, 100uSec tick)
-  radtx_setup(7, 10, 0, 140);
+  //Transmit on pin IRLED, 10 repeats,no invert, 100uSec tick)
+  pinMode(LED, OUTPUT);
+  radtx_setup(IRLED, 10, 0, 140);
+  Particle.function("send", sendMsg);
 }
 
 void loop() {
-  if (radtx_free()) {
-    radtx_send(msg);
-    timeout = millis();
-  }
-  while(!radtx_free() && millis() < (timeout + 1000)) {
-    delay(10);
-  }
-  timeout = millis() - timeout;
-  delay(10000);
 }
 
+int sendMsg(String command)
+{
+  int msgIndex = atoi(command);
+  
+  // look for the matching command
+  if(msgIndex >= 0 && msgIndex < MAX_MSGS)
+  {
+    timeout = millis();
+    while(!radtx_free() && millis() < (timeout + 1000)) {
+      delay(10);
+    }
+    digitalWrite(LED, HIGH);
+    radtx_send(&msg[msgIndex * MSG_LEN]);
+    digitalWrite(LED, LOW);
+    return 0;
+  }
+  else return -1;
+}
